ef3b2e67e6a6dbd039a5605ccd704906bcf65cd7
William Han <willhan327@outlook.com>
2022-02-21 02:30:35 -0800

    completed ps 0

--- MIT 6.006/ps0-template/count_long_subarray.py
+++ MIT 6.006/ps0-template/count_long_subarray.py
@@ -3,11 +3,31 @@ def count_long_subarray(A):
     Input:  A     | Python Tuple of positive integers
     Output: count | number of longest increasing subarrays of A
     '''
-    count = 0
+    n = len(A)
+    current = 1 #length of longest increasing subarray ending at A[i-1]
+    length = 1  #length of longest increasign subarray in A[:1]
+    count = 1   # number of longest increasing subarray in A[:i]
 
-    for
+    for i in range(1,n):
 
-    
+        if A[i-1] < A[i]:
+
+            current += 1
+
+        else:
+
+            current = 1
+
+        if current == length:
+
+            count += 1
+
+        elif current > length:
+
+            length = current
+
+            count = 1
+            
 
 
     
