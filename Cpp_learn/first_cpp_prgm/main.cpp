5b5f6272e0d66f76d1d55313b086a3fc5a903445
William Han <willhan327@outlook.com>
2022-02-23 17:12:14 -0800

    first program

---
+++ Cpp_learn/first_cpp_prgm/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream> //iostream is library
+
+int main(){
+
+    std::cout << "William Han" << std::endl;
+    std::cout << "William Han" << std::endl;
+    std::cout << "William Han" << std::endl;
+    std::cout << "William Han" << std::endl;
+    std::cout << "William Han" << std::endl;
+    std::cout << "William Han" << std::endl;
+    std::cout << "William Han" << std::endl;
+    return 0;
+}
+
