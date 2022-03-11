2fcc0dd3874613966d1999d89fa0b0b31425e104
William Han <willhan327@outlook.com>
2022-03-11 00:14:44 -0800

    boolean

--- Cpp_learn/boolean/main.cpp
+++ Cpp_learn/boolean/main.cpp
@@ -1,7 +1,40 @@
 #include <iostream> 
-#include <compare>
+
 
 int main(){
-    auto result = (10 <=> 20) > 0;
-    std::cout << result << std::endl;
+    
+    bool red_light {false};
+    bool green_light{true};
+
+    if(red_light == true){
+        std::cout << "stop!" << std::endl;
+    }else{
+        std::cout << "go through!" << std::endl;
+    }
+
+
+
+    if(green_light){
+        std::cout << "The light is green!" << std::endl;
+    }else{
+        std::cout << "the light is not green!" << std::endl;
+
+    }
+
+    //sizeof() takes 1 byte
+    std::cout << "sizeof(bool): " << sizeof(bool) << std::endl;
+
+    //printing out a bool
+    //1 --> true
+    //0 --> false
+    std::cout << std::endl;
+    std::cout << "red_light: " << red_light << std::endl;
+    std::cout << "green_light: " << green_light << std::endl;
+
+    std::cout << std::boolalpha;
+
+    std::cout << "red_light: " << red_light << std::endl;
+    std::cout << "green_light: " << green_light << std::endl;
+
+    return 0;
 }
