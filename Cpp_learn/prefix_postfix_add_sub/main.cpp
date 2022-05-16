3ac34fee1fda0dbeefe44d8f924b3b19356bacf5
William Han <willhan327@outlook.com>
2022-05-16 00:11:02 -0700

    prefix and postfix

--- Cpp_learn/prefix_postfix_add_sub/main.cpp
+++ Cpp_learn/prefix_postfix_add_sub/main.cpp
@@ -1,7 +1,49 @@
 #include <iostream> 
-#include <compare>
 
 int main(){
-    auto result = (10 <=> 20) > 0;
-    std::cout << result << std::endl;
+    
+    int value {5};
+
+    value = value + 1;
+    std::cout << "the value is: " << value << std::endl;
+
+    value = 5;
+    value = value -1;
+    std::cout << "the value is : " << value << std::endl;
+
+    std::cout << "=====Postfix increment and decrement=======" << std::endl;
+
+    value = 5;
+    std::cout << "the value is (incrementing) : " << value++ << std::endl; //increment by one
+    std::cout << "the value is " << value << std::endl;
+
+    std::cout << std::endl;
+
+    value = 5;
+
+    std::cout <<"The value is (decrementing): " << value-- <<std::endl;
+    std::cout << "the value is: " << value << std::endl;
+
+    std::cout << "=====Prefix increment and decrement=======" << std::endl;
+
+    value = 5;
+    
+    ++value;
+    std::cout << "the value is (prefix++) :" << value << std::endl;
+
+    value = 5;
+    std::cout << " the value is (prefix++ in place) : " << ++value << std::endl;
+
+    std::cout << std::endl;
+
+    //Prefix : Decrementing
+
+    value = 5;
+    --value;
+    std::cout << "the value is (predix--) : " << value << std::endl;
+
+    value = 5;
+    std::cout << "the value is (predix-- in place) : " << --value << std::endl;
+
+    return 0;
 }
