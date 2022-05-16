9802a14fa1c80681d03cdcdcdb0b2132869b2adc
William Han <willhan327@outlook.com>
2022-05-15 23:53:58 -0700

    precendece

--- Cpp_learn/precedence_associativity/main.cpp
+++ Cpp_learn/precedence_associativity/main.cpp
@@ -1,7 +1,23 @@
 #include <iostream> 
-#include <compare>
+
 
 int main(){
-    auto result = (10 <=> 20) > 0;
-    std::cout << result << std::endl;
+
+    //precedence: which operation to do first
+    //associativity: which direction or which order 
+
+    int a {6};
+    int b {3};
+    int c {8};
+    int d {9};
+    int e {3};
+    int f {2};
+    int g {5};
+
+    int result = a + b * c -d/e -f + g;
+
+    std::cout << "result : " << result << std::endl;
+
+
+    return 0;
 }
