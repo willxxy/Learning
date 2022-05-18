234b31fa00ce1d5a6d59b92d40268956a373a079
William Han <willhan327@outlook.com>
2022-05-18 00:43:16 -0700

    relational operators

--- Cpp_learn/relational_operators/main.cpp
+++ Cpp_learn/relational_operators/main.cpp
@@ -17,5 +17,15 @@ std::cout << std::boolalpha; // Make bool show up as true/false instead of 1/0
 
 std::cout << "number1 < number2: " << (number1 < number2) << std::endl;
 
+std::cout << "number1 <= number2: " << (number1 <= number2) << std::endl;
+
+std::cout << "number1 > number2: " << (number1 > number2) << std::endl;
+
+std::cout << "number1 >= number2: " << (number1 >= number2) << std::endl;
+
+std::cout << "number1 == number2: " << (number1 == number2) << std::endl;
+
+std::cout << "number1 != number2: " << (number1 != number2) << std::endl;
+
     return 0;    
 }
