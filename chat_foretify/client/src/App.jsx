7fd67e5a21e3c3f1c2d1f50441f9a908dd19a115
William Han <willhan327@outlook.com>
2022-06-20 05:13:23 -0700

    chat

---
+++ chat_foretify/client/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { StreamChat } from 'stream-chat';
+import { Chat } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+
+import { ChannelListContainer, ChannelContainer } from './components';
+
+const apiKey = 'ysk7qjpbrrut';
+
+const client = StreamChat.getInstance(apiKey);
+
+const App = () => {
+  return (
+    <div className="app_wrapper">
+        <Chat client={client} theme="team light">
+            <ChannelListContainer
+            />
+            <ChannelContainer
+            />
+
+        </Chat>
+        <h1>GeoLocation Chat App</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ chat_foretify/client/src/components/ChannelListContainer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const ChannelListContainer = () => {
+  return (
+    <div>ChannelListContainer</div>
+  );
+}
+
+export default ChannelListContainer;
