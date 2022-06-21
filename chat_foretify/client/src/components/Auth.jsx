07b823d157c988dd501fc79bbe6eb8ad474cf5cb
William Han <willhan327@outlook.com>
2022-06-21 00:17:25 -0700

    auth

--- chat_foretify/client/src/components/Auth.jsx
+++ chat_foretify/client/src/components/Auth.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react'
-import Cookiese from 'universal-cookie'
+import Cookies from 'universal-cookie'
 import axios from 'axios';
 
 import signinImage from '../assets/signup.jpg'
+
+const cookies = new Cookies();
 
 const initialState = {
     fullName: '',
@@ -21,9 +23,29 @@ const Auth = () => {
         setForm({ ... form, [e.target.name]: e.target.value});
     }
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        console.log(form);
+        
+        const { fullName, username, password, phoneNumber, avatarURL } = form;
+
+        const URL = 'http://localhost:5001/auth';
+
+        const {data: { token, userId, hashedPassword}} = await axios.post(`${URL}/${isSignup ? 'signup' : 'login'}`, {
+            username, password, fullName, phoneNumber, avatarURL
+        })
+
+        cookies.set('token', token);
+        cookies.set('username', username);
+        cookies.set('fullName', fullName);
+        cookies.set('userId', userId);
+
+        if(isSignup) {
+            cookies.set('phoneNumber', phoneNumber);
+            cookies.set('avatarURL', avatarURL);
+            cookies.set('hashedPassword', hashedPassword);
+        }
+
+        window.location.reload();
     }
 
     const switchMode = () => {
