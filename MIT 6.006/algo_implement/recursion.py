5b5c780e3de6a6fdd25cfdd32b41a92e4bf17de7
William Han <willhan327@outlook.com>
2022-08-20 20:03:54 -0700

    recursion func

--- MIT 6.006/algo_implement/recursion.py
+++ MIT 6.006/algo_implement/recursion.py
@@ -1,9 +1,25 @@
 #---recursion
+import math
+import time
 
 def f(n):
     if n == 0 or n == 1:
         return 1
     else:
         return n * f(n-1)
+
+def dosomething(n):
+    if n<=2:
+        return 1
+    else:
+        return (dosomething(math.floor(math.sqrt(n)))) + n
+
+if __name__  == '__main__':
+    start_time = time.time()
+
+    print(dosomething(70000))
+    print()
+    print("--- %s seconds ---" % (time.time() - start_time))
+
 
     
